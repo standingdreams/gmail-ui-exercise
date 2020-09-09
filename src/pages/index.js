1b8c9154d335388ef3b4edd525991446476d6a2f
Douglas Rogers <designer.drogers@gmail.com>
2020-09-09 18:52:49 -0400

    Completed star and trash functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,26 @@ import emailData from "../data/emails.json"
 import Layout from "../components/layout"
 
 function reducer(state, action) {
-  const { activeTag } = state
+  const { messages, star, trash } = state
 
   switch(action.type) {
     case "tag":
       return {
         ...state,
         activeTag: action.payload
+      }
+
+    case "star":
+      return {
+        ...state,
+        star: star.includes(action.payload) ? star.filter(item => item !== action.payload) : [...star, action.payload]
+      }
+
+    case "trash":
+      return {
+        ...state,
+        messages: messages.filter(message => message.id !== action.payload.id),
+        trash: [...trash, action.payload]
       }
 
     default:
@@ -22,12 +35,6 @@ function reducer(state, action) {
 
 const IndexPage = () => {
   const [activeNav, setActiveNav] = useState('inbox')
-  const initialState = {
-    activeTag: '',
-  }
-
-  const [state, dispatch] = useReducer(reducer, initialState)
-
   const data = useStaticQuery(graphql`
     {
       allDataJson {
@@ -47,6 +54,15 @@ const IndexPage = () => {
     }
   `)
 
+  const initialState = {
+    activeTag: '',
+    messages: data.allDataJson.edges[0].node.messages,
+    star: [],
+    trash: [],
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <Layout title="Home">
       <section className="c-inbox">
@@ -55,10 +71,10 @@ const IndexPage = () => {
             <nav className="c-sidebar__nav">
               <ul>
                 <li>
-                  <button className={`c-sidebar__navItem c-sidebar__navItem--inbox${activeNav === 'inbox' ? ' c-sidebar--active' : ''}`} onClick={() => {setActiveNav('inbox')}}>Inbox <span>{ emailData.messages.length }</span></button>
+                  <button className={`c-sidebar__navItem c-sidebar__navItem--inbox${activeNav === 'inbox' ? ' c-sidebar--active' : ''}`} onClick={() => {setActiveNav('inbox')}}>Inbox <span>{ state.messages.length }</span></button>
                 </li>
                 <li>
-                  <button className={`c-sidebar__navItem c-sidebar__navItem--starred c-sidebar__navItem--interior${activeNav === 'starred' ? ' c-sidebar--active' : ''}`} onClick={() => {setActiveNav('starred'); dispatch({ type:"tag", payload: "starred" })}}>Starred</button>
+                  <button className={`c-sidebar__navItem c-sidebar__navItem--starred c-sidebar__navItem--interior${activeNav === 'starred' ? ' c-sidebar--active' : ''}`} onClick={() => {setActiveNav('starred'); dispatch({ type:"tag", payload: "starred" })}}>Starred <span>{ state.star.length > 0 ? state.star.length : '' }</span></button>
                 </li>
                 <li>
                   <button className={`c-sidebar__navItem c-sidebar__navItem--tags c-sidebar__navItem--interior${activeNav === 'travel' ? ' c-sidebar--active' : ''}`} onClick={() => {setActiveNav('travel'); dispatch({ type:"tag", payload: "travel" })}}><span></span>Travel</button>
@@ -67,7 +83,7 @@ const IndexPage = () => {
                   <button className={`c-sidebar__navItem c-sidebar__navItem--tags c-sidebar__navItem--interior${activeNav === 'work' ? ' c-sidebar--active' : ''}`} onClick={() => {setActiveNav('work'); dispatch({ type:"tag", payload: "work" })}}><span></span>Work</button>
                 </li>
                 <li>
-                  <button className={`c-sidebar__navItem c-sidebar__navItem--trash c-sidebar__navItem--interior${activeNav === 'trash' ? ' c-sidebar--active' : ''}`} onClick={() => {setActiveNav('trash'); dispatch({ type: "tag", payload: "trash" })}}>Trash</button>
+                  <button className={`c-sidebar__navItem c-sidebar__navItem--trash c-sidebar__navItem--interior${activeNav === 'trash' ? ' c-sidebar--active' : ''}`} onClick={() => {setActiveNav('trash'); dispatch({ type: "tag", payload: "trash" })}}>Trash <span>{ state.trash.length > 0 ? state.trash.length : '' }</span></button>
                 </li>
               </ul>
             </nav>
@@ -75,20 +91,21 @@ const IndexPage = () => {
           <div className="emailList">
             <table className="emailList__table">
               <tbody>
-                {data.allDataJson.edges[0].node.messages.filter(message => (activeNav !== 'inbox' ? message.tags.includes(state.activeTag) : true)).map((email, index) => (
+                {state.messages.filter(message => (activeNav !== 'inbox' ? message.tags.includes(state.activeTag) : true)).map(email => (
                   <tr className={`emailList__item`} key={email.id}>
-                    <td className="emailList__sender">{ email.sender }</td>
+                    <td className={`emailList__sender`}>
+                      <button className={`emailList__star${state.star.includes(email) ? ' emailList__star--active' : ''}`} onClick={() => {dispatch({type: 'star', payload: email})}}></button>
+                      { email.sender }
+                    </td>
                     <td className="emailList__details">
                       <div className="emailList__subject">
                         { email.subject }
                       </div>
                       <div className="emailList__snippet" dangerouslySetInnerHTML={{__html:email.body}}/>
                       <div className="emailList__tags">
-                        {
-                          <ul>
-                            { email.tags.map((tag, index) => (<li key={ index }>{ tag }</li>)) }
-                          </ul>
-                        }
+                        { <ul>
+                          { email.tags.map((tag, index) => (<li key={ index }>{ tag }</li>)) }
+                        </ul> }
                       </div>
                     </td>
                     <td className="emailList__date">{ moment(email.date).format('MM/DD/YYYY') }</td>
@@ -96,8 +113,7 @@ const IndexPage = () => {
                       <nav>
                         <ul>
                           <li>
-                            <button>Delete</button>
-                            <button>Star</button>
+                            <button className={`emailList__navBtn emailList__navBtn--trash`} onClick={() => {dispatch({type: 'trash', payload: email})}}>Delete</button>
                           </li>
                         </ul>
                       </nav>
