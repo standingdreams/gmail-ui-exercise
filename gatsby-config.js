e64b65d93f99065f56a8152ff170867cf94851c7
Douglas Rogers <designer.drogers@gmail.com>
2020-09-09 18:45:20 -0400

    Initial start
    Import data into index
    Output data to table
    Installed Gatsby plugins: Google Fonts, SASS, dotenv, JSON Transformer
    Reordered layout
    Added SCSS starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `SalesLoft | UI Exercise`,
+    description: `A responsive single page app that is based on Gmail's web interface.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -13,6 +13,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -27,8 +28,24 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto\:400,500,700`
+        ],
+        display: 'swap'
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `emails`,
+        path: `${__dirname}/src/data/`,
+        plugins: [
+          `gatsby-transformer-json`
+        ]
+      }
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,25 @@
 import React from "react"
-import { Link } from "gatsby"
+import emailData from "../data/emails.json"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  <Layout title="Home">
+    <table>
+      {emailData.messages.map((email, index) => (
+        <tr key={email.id}>
+          <td>{ email.sender }</td>
+          <td>{
+            <ul>
+              { email.tags.map((tag, index) => (<li key={ index }>{ tag }</li>)) }
+            </ul>
+           }</td>
+          <td>{ email.subject }</td>
+          <td dangerouslySetInnerHTML={{__html:email.body}}></td>
+          <td>{ email.date }</td>
+        </tr>
+      ))}
+    </table>
   </Layout>
 )
 
